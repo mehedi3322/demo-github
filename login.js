2cd1412ab7a71508af6bb83780ea2024c4435f3a
mehedi3322 <mehedihasan88990011@gmail.com>
2026-03-09 20:55:59 +0600

    make a sign up page functional

--- login.js
+++ login.js
@@ -1,1 +1,17 @@
-console.log("hello github");
+function loginUser(){
+
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+if(username === "admin" && password === "admin123"){
+
+window.location.href = "issue.html";
+
+}
+else{
+
+alert("Invalid Username or Password");
+
+}
+
+}
